2e415c450686e02d83e6807fa4fd13e9eccdfb3c
Yaroslav <cyrax1986@gmail.com>
2017-12-06 14:14:13 +0200

    Update stm32f0xx_it.c

--- stm32f0xx_it.c
+++ stm32f0xx_it.c
@@ -41,16 +41,7 @@
 
 /* External variables --------------------------------------------------------*/
 extern CAN_HandleTypeDef hcan;
-extern DMA_HandleTypeDef hdma_i2c1_rx;
-extern DMA_HandleTypeDef hdma_i2c1_tx;
-extern I2C_HandleTypeDef hi2c1;
-extern TIM_HandleTypeDef htim16;
-extern TIM_HandleTypeDef htim17;
-extern DMA_HandleTypeDef hdma_usart1_rx;
-extern DMA_HandleTypeDef hdma_usart1_tx;
-extern UART_HandleTypeDef huart1;
 
-extern TIM_HandleTypeDef htim6;
 
 /******************************************************************************/
 /*            Cortex-M0 Processor Interruption and Exception Handlers         */ 
@@ -91,123 +82,6 @@ void HardFault_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f0xx.s).                    */
 /******************************************************************************/
-
-/**
-* @brief This function handles DMA1 channel 1 interrupt.
-*/
-void DMA1_Ch1_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA1_Ch1_IRQn 0 */
-
-  /* USER CODE END DMA1_Ch1_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_usart1_rx);
-  /* USER CODE BEGIN DMA1_Ch1_IRQn 1 */
-
-  /* USER CODE END DMA1_Ch1_IRQn 1 */
-}
-
-/**
-* @brief This function handles DMA1 channel 2 to 3 and DMA2 channel 1 to 2 interrupts.
-*/
-void DMA1_Ch2_3_DMA2_Ch1_2_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA1_Ch2_3_DMA2_Ch1_2_IRQn 0 */
-
-  /* USER CODE END DMA1_Ch2_3_DMA2_Ch1_2_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_i2c1_tx);
-  HAL_DMA_IRQHandler(&hdma_i2c1_rx);
-  /* USER CODE BEGIN DMA1_Ch2_3_DMA2_Ch1_2_IRQn 1 */
-
-  /* USER CODE END DMA1_Ch2_3_DMA2_Ch1_2_IRQn 1 */
-}
-
-/**
-* @brief This function handles DMA1 channel 4 to 7 and DMA2 channel 3 to 5 interrupts.
-*/
-void DMA1_Ch4_7_DMA2_Ch3_5_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA1_Ch4_7_DMA2_Ch3_5_IRQn 0 */
-
-  /* USER CODE END DMA1_Ch4_7_DMA2_Ch3_5_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_usart1_tx);
-  /* USER CODE BEGIN DMA1_Ch4_7_DMA2_Ch3_5_IRQn 1 */
-
-  /* USER CODE END DMA1_Ch4_7_DMA2_Ch3_5_IRQn 1 */
-}
-
-/**
-* @brief This function handles TIM6 global and DAC channel underrun error interrupts.
-*/
-void TIM6_DAC_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
-
-  /* USER CODE END TIM6_DAC_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim6);
-  /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
-
-  /* USER CODE END TIM6_DAC_IRQn 1 */
-}
-
-/**
-* @brief This function handles TIM16 global interrupt.
-*/
-void TIM16_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM16_IRQn 0 */
-
-  /* USER CODE END TIM16_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim16);
-  /* USER CODE BEGIN TIM16_IRQn 1 */
-
-  /* USER CODE END TIM16_IRQn 1 */
-}
-
-/**
-* @brief This function handles TIM17 global interrupt.
-*/
-void TIM17_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM17_IRQn 0 */
-
-  /* USER CODE END TIM17_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim17);
-  /* USER CODE BEGIN TIM17_IRQn 1 */
-
-  /* USER CODE END TIM17_IRQn 1 */
-}
-
-/**
-* @brief This function handles I2C1 event global interrupt / I2C1 wake-up interrupt through EXTI line 23.
-*/
-void I2C1_IRQHandler(void)
-{
-  /* USER CODE BEGIN I2C1_IRQn 0 */
-
-  /* USER CODE END I2C1_IRQn 0 */
-  if (hi2c1.Instance->ISR & (I2C_FLAG_BERR | I2C_FLAG_ARLO | I2C_FLAG_OVR)) {
-    HAL_I2C_ER_IRQHandler(&hi2c1);
-  } else {
-    HAL_I2C_EV_IRQHandler(&hi2c1);
-  }
-  /* USER CODE BEGIN I2C1_IRQn 1 */
-
-  /* USER CODE END I2C1_IRQn 1 */
-}
-
-/**
-* @brief This function handles USART1 global interrupt / USART1 wake-up interrupt through EXTI line 25.
-*/
-void USART1_IRQHandler(void)
-{
-  /* USER CODE BEGIN USART1_IRQn 0 */
-
-  /* USER CODE END USART1_IRQn 0 */
-  HAL_UART_IRQHandler(&huart1);
-  /* USER CODE BEGIN USART1_IRQn 1 */
-
-  /* USER CODE END USART1_IRQn 1 */
-}
 
 /**
 * @brief This function handles HDMI-CEC and CAN global interrupts / HDMI-CEC wake-up interrupt through EXTI line 27.
